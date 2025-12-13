d952d6e58e9e25b7219e05850cc8fdbfbb20e601
Zaelani <bahar32harz@gmail.com>
2025-12-13 21:36:54 +0700

    update paragraf and add icon ,

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,11 @@ function App() {
 
   return (
     <div className="w-full h-screen flex flex-col items-center justify-center">
+      <img className="rounded-3xl shadow-2xl mb-3" src="/public/images/busy-cat.gif" />
       <h1 className="text-6xl mb-4 font-bold">
         Halo Dunya !
       </h1>
-      <p>Website sedang pengembangan . . . (～￣▽￣)～</p>
+      <p>Website sedang dalam pengembangan . . . (～￣▽￣)～</p>
     </div>
   )
 }
