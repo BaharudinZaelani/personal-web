aeab368837fe94bdaa073f056fa6ced598e3468c
Zaelani <bahar32harz@gmail.com>
2025-12-13 22:46:24 +0700

    fixing components not rendered !.

--- src/main.jsx
+++ src/main.jsx
@@ -5,14 +5,12 @@ import { BrowserRouter, Route, Routes } from "react-router";
 
 // Pages
 import Home from "./pages/Home.jsx";
-import ExampleMotion from "./pages/ExampleMotion.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/example-motion" element={<ExampleMotion />} />
       </Routes>
     </BrowserRouter>
   </StrictMode>,
