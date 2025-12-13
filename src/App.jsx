29165cab5df00cbdef011fe6b570df2af7df3645
Zaelani <bahar32harz@gmail.com>
2025-12-13 20:59:01 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+function App() {
+
+  return (
+    <div className="w-full h-screen flex flex-col items-center justify-center">
+      <h1 className="text-6xl mb-4 font-bold">
+        Halo Dunya !
+      </h1>
+      <p>Website sedang pengembangan . . . (～￣▽￣)～</p>
+    </div>
+  )
+}
+
+export default App
